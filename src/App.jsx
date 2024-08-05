0f32f74354e343dba69318937eb47c7f33948648
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-05 19:07:20 +0700

    feat : save data to localStorage and change name useStae addBook to addTaskForm

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,43 @@ import { Header } from "./component/Header";
 import { useState } from "react";
 import TasksList from "./component/Tasks";
 import TaskForm from "./component/Tasks/ModalTaskForm";
+import { useEffect } from "react";
+
 function App() {
-  const [addBook, setAddBook] = useState(false);
-  const [tasks, setTasks] = useState([]);
+  const [addTaskForm, setAddTaskForm] = useState(false);
   const [task, setTask] = useState("");
   const [deadline, setDeadline] = useState("");
   const [status, setStatus] = useState("");
-  console.log(tasks);
+
+  const [tasks, setTasks] = useState(() => {
+    const tasksFromLocalStorage = localStorage.getItem("tasks");
+    if (tasksFromLocalStorage) {
+      return JSON.parse(tasksFromLocalStorage);
+    } else {
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    if (tasks === undefined) {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+  }, [tasks]);
 
   return (
     <>
       <div className="relative flex justify-center w-screen h-screen bg-gray-300 font-kreon text-primary">
         <div className="w-1/2">
           <Header />
-          <TasksList addBook={addBook} setAddBook={setAddBook} tasks={tasks} />
+          <TasksList
+            addTaskForm={addTaskForm}
+            setAddTaskForm={setAddTaskForm}
+            tasks={tasks}
+            setTasks={setTasks}
+          />
         </div>
 
-        {addBook && (
+        {addTaskForm && (
           <TaskForm
             tasks={tasks}
             setTasks={setTasks}
@@ -28,7 +48,7 @@ function App() {
             setDeadline={setDeadline}
             status={status}
             setStatus={setStatus}
-            setAddBook={setAddBook}
+            setAddTaskForm={setAddTaskForm}
           />
         )}
       </div>
