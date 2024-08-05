2468ee4a55ba712eab8e7e99628336cd0177904d
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-05 17:58:27 +0700

    feat : add config for tailwindcss on screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,20 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens: {
+        mobile: "640px", // Mobile
+        laptop: "1024px", // Laptop
+        desktop: "1280px", // Desktop
+      },
       fontFamily: {
         kreon: ["Kreon", "sans-serif"],
       },
       colors: {
+        gray: "#929292",
         primary: "#515151",
         green: "#9AE2A5",
         red: "#EB9393",
         blue: "#D1EDF5",
-        gray: "#C0C0C0",
       },
     },
   },
