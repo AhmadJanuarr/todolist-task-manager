6e953f60469b86d68e6f3781648e5bc948a620b8
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-05 17:59:27 +0700

    refactor : changes to id writing and addition of useEffect

--- src/component/Tasks/task.jsx
+++ src/component/Tasks/task.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import { useState } from "react";
 export default function Task({ tasks }) {
   const [selectedTasks, setSelectedTasks] = useState([]);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("tasks"));
+    if (items) setItems(items);
+  }, []);
 
   const handleCheckBoxChange = (taskId) => {
     if (selectedTasks.includes(taskId)) {
@@ -10,24 +21,27 @@ export default function Task({ tasks }) {
     }
   };
 
-  const listItem = tasks.map((task, index) => {
+  const listItem = items.map((index, items) => {
+    const { id, task: taskName, deadline, status } = items;
+    const truncatedTaskName =
+      taskName.length > 20 ? taskName.substring(0, 20) + "..." : taskName;
     return (
-      <tr key={index} className="border-b ">
-        <td className="pl-5 " width={"5%"}>
+      <tr key={index} className="border-b text-[16px]">
+        <td className="px-5 pl-5" width={"5%"}>
           <input
             type="checkbox"
-            checked={selectedTasks.includes(task.id)}
-            onChange={() => handleCheckBoxChange(task.id)}
+            checked={selectedTasks.includes(id)}
+            onChange={() => handleCheckBoxChange(id)}
           />
         </td>
         <td className="py-5" width={"55%"}>
-          {task.task}
+          {truncatedTaskName}
         </td>
         <td className="py-5" width={"20%"}>
-          {task.deadline}
+          {deadline}
         </td>
         <td className="py-5" width={"20%"}>
-          {task.status}
+          {status}
         </td>
       </tr>
     );
@@ -36,7 +50,7 @@ export default function Task({ tasks }) {
   return (
     <table className="w-full text-xl text-left ">
       <thead>
-        <tr>
+        <tr className="text-lg text-gray">
           <th width="60%" colSpan={2}>
             Task Name
           </th>
