0e98fc8b154a8593d4e72b62905635c58ad9f177
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-08 23:50:29 +0700

    feat : provide props operands for mobilecomponent

--- src/component/Tasks/task.jsx
+++ src/component/Tasks/task.jsx
@@ -1,12 +1,21 @@
 import DesktopComponent from "./ResponsiveComponent/DesktopComponent";
 import MobileComponent from "./ResponsiveComponent/MobileComponent";
 
-export default function Task({ tasks }) {
+export default function Task({
+  tasks,
+  setTasks,
+  selectedTasksById,
+  setSelectedTasksById,
+}) {
   return (
     <div className="overflow-y-auto ">
-      <div className="w-full border h-[26em]">
+      <div className="w-full  h-[26em]">
         {/* <DesktopComponent tasks={tasks} /> */}
-        <MobileComponent tasks={tasks} />
+        <MobileComponent
+          tasks={tasks}
+          setSelectedTasksById={setSelectedTasksById}
+          selectedTasksById={selectedTasksById}
+        />
       </div>
     </div>
   );
