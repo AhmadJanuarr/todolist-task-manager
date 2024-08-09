1c39e6aef39247ef734fdb972e013025a670ed5c
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-09 22:13:59 +0700

    fix : fix code

--- src/component/Tasks/ModalTaskForm.jsx
+++ src/component/Tasks/ModalTaskForm.jsx
@@ -11,8 +11,8 @@ export default function TaskForm({
   setAddTaskForm,
 }) {
   return (
-    <div className="absolute flex flex-col items-center justify-center h-screen w-screen">
-      <div className="py-6 px-7 laptop:w-[36em] w-full">
+    <div className="absolute flex flex-col items-center justify-center w-screen h-screen">
+      <div className="py-6 px-7 laptop:w-[36em] w-full z-20	">
         <FormInput
           tasks={tasks}
           setTasks={setTasks}
