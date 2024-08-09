3c6434a593a07bdfed99e0cf849fb62cfabaf5eb
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-09 22:14:38 +0700

    feat : add components desktop and props

--- src/component/Tasks/task.jsx
+++ src/component/Tasks/task.jsx
@@ -3,14 +3,17 @@ import MobileComponent from "./ResponsiveComponent/MobileComponent";
 
 export default function Task({
   tasks,
-
   selectedTasksById,
   setSelectedTasksById,
 }) {
   return (
-    <div className="overflow-y-auto ">
-      <div className="w-full h-[26em]">
-        {/* <DesktopComponent tasks={tasks} /> */}
+    <div className="overflow-y-scroll ">
+      <div className="w-full h-[26em] laptop:h-[20em]">
+        <DesktopComponent
+          tasks={tasks}
+          setSelectedTasksById={setSelectedTasksById}
+          selectedTasksById={selectedTasksById}
+        />
         <MobileComponent
           tasks={tasks}
           setSelectedTasksById={setSelectedTasksById}
