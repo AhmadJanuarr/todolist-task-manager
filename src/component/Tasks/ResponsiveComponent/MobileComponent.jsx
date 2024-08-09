001b2c1a0fc71ea2db9f70f98b2f0b86e2c57c77
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-09 10:31:53 +0700

    refactor : update ui for task

--- src/component/Tasks/ResponsiveComponent/MobileComponent.jsx
+++ src/component/Tasks/ResponsiveComponent/MobileComponent.jsx
@@ -14,35 +14,45 @@ export default function MobileComponent({
   const listItem = tasks.map((task) => {
     const { id, task: taskName, deadline, status } = task;
 
+    const className = "px-2 text-center border rounded-xl";
+    const statusClasName =
+      status === "In Progress"
+        ? `text-cyan-500 ${className}`
+        : status === "Not Progress"
+        ? `text-rose-500  ${className}`
+        : `text-emerald-500  ${className}`;
     return (
       <table
-        className="w-full mb-5 text-left border border-gray-300 rounded text-md bg-grayDark"
-        style={{ borderSpacing: "10px", borderCollapse: "separate" }} // Menambahkan jarak antar sel
+        className="w-full mb-5 text-left border-separate border-gray-300 rounded text-md bg-grayDark border-spacing-2"
         key={id}
       >
-        <tr>
-          <td className="px-2 text-gray " width={"5%"} rowSpan={4}>
-            <input
-              type="checkbox"
-              checked={selectedTasksById.includes(id)}
-              onChange={() => handleCheckBoxChange(id)}
-            />
-          </td>
-        </tr>
-        <tr className="text-[12px] ">
-          <th className="text-gray">Task Name</th>
-          <td width={"70%"}>{taskName}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td className="px-2 text-gray " width={"5%"} rowSpan={4}>
+              <input
+                type="checkbox"
+                checked={selectedTasksById.includes(id)}
+                onChange={() => handleCheckBoxChange(id)}
+              />
+            </td>
+          </tr>
+          <tr className="text-[12px]">
+            <th className="text-gray">Task Name</th>
+            <td width={"70%"}>{taskName}</td>
+          </tr>
 
-        <tr className=" text-[12px] ">
-          <th className="text-gray">Deadline</th>
-          <td>{deadline}</td>
-        </tr>
+          <tr className=" text-[12px] ">
+            <th className="text-gray">Deadline</th>
+            <td>{deadline}</td>
+          </tr>
 
-        <tr className="text-[12px] ">
-          <th className="text-gray">Status</th>
-          <td>{status}</td>
-        </tr>
+          <tr className="text-[12px] ">
+            <th className="text-gray">Status</th>
+            <td>
+              <button className={statusClasName}>●{status}</button>
+            </td>
+          </tr>
+        </tbody>
       </table>
     );
   });
