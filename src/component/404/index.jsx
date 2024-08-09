fcd6a6ba45239f471d04a8e55cd85c6c2a6e34c9
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-09 10:30:59 +0700

    feat : create file error not found

---
+++ src/component/404/index.jsx
@@ -0,0 +1,8 @@
+export default function ErrorNotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p className="text-2xl font-bold">Page Not Found</p>
+    </div>
+  );
+}
