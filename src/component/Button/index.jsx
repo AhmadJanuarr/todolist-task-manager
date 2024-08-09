3566b4d7426aec06151604f19ff8d01641511c29
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-09 22:12:38 +0700

    fix : fix on button all devices

--- src/component/Button/index.jsx
+++ src/component/Button/index.jsx
@@ -2,7 +2,7 @@ export default function Button({ children, variant, onClick, type }) {
   const variantClasses = variant === "danger" ? "bg-red" : "bg-green";
   return (
     <button
-      className={`bg-green px-4 py-1 rounded text-white text-[11px] ${variantClasses}`}
+      className={`bg-green px-4 py-1 rounded text-white text-[11px] laptop:text-[16px] laptop:px-6 ${variantClasses}`}
       onClick={onClick}
       type={type}
     >
