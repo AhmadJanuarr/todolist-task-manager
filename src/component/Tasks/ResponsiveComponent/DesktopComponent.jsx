6f5ad633dd75cf63adc186be3445a0ff697c2a22
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-09 22:15:47 +0700

    feat & fix : fix code and update code

--- src/component/Tasks/ResponsiveComponent/DesktopComponent.jsx
+++ src/component/Tasks/ResponsiveComponent/DesktopComponent.jsx
@@ -1,57 +1,67 @@
-import { useState } from "react";
-
-export default function DesktopComponent({ tasks }) {
-  const [selectedTasks, setSelectedTasks] = useState([]);
-
+export default function DesktopComponent({
+  tasks,
+  selectedTasksById,
+  setSelectedTasksById,
+}) {
   const handleCheckBoxChange = (taskId) => {
-    if (selectedTasks.includes(taskId)) {
-      setSelectedTasks(selectedTasks.filter((id) => id !== taskId));
+    if (selectedTasksById.includes(taskId)) {
+      setSelectedTasksById(selectedTasksById.filter((id) => id !== taskId));
     } else {
-      setSelectedTasks([...selectedTasks, taskId]);
+      setSelectedTasksById([...selectedTasksById, taskId]);
     }
   };
-
-  console.log(selectedTasks);
 
   const listItem = tasks.map((task) => {
     const { id, task: taskName, deadline, status } = task;
     const truncatedTaskName =
       taskName.length > 20 ? taskName.substring(0, 20) + "..." : taskName;
+
+    const className = "px-3 text-center rounded-xl";
+    const statusClassName =
+      status === "In Progress"
+        ? `bg-blue text-zinc-600 border-none ${className}`
+        : status === "Not Progress"
+        ? `bg-gray text-zinc-600 border-none ${className}`
+        : `bg-green text-zinc-600 border-none ${className}`;
+
     return (
-      <tr key={id} className="bg-grayDark text-[12px] ">
-        <td className="px-2 text-gray" width={"5%"}>
+      <tr key={id} className="hover:bg-grayDark text-[14px] py-2">
+        <td className="px-2 py-2 text-gray" width="5%">
           <input
             type="checkbox"
-            checked={selectedTasks.includes(id)}
+            checked={selectedTasksById.includes(id)}
             onChange={() => handleCheckBoxChange(id)}
           />
         </td>
-        <td className="py-5" width={"55%"}>
+        <td width="55%" className="py-2">
           {truncatedTaskName}
         </td>
-        <td className="py-5" width={"20%"}>
+        <td width="20%" className="py-2">
           {deadline}
         </td>
-        <td className="py-5" width={"20%"}>
-          {status}
+        <td width="20%">
+          <button className={`${statusClassName} `}>● {status}</button>
         </td>
       </tr>
     );
   });
 
-  const responsiveLaptop = "laptop:flex laptop:flex-col laptop:block";
   return (
-    <table className={`hidden w-full text-xl text-left ${responsiveLaptop}`}>
-      <thead className="border">
-        <tr className="laptop:text-[14px] text-gray flex">
-          <th width="60%" colSpan={2}>
-            Task Name
-          </th>
-          <th width="20%">Deadline</th>
-          <th width="20%">Status</th>
-        </tr>
-      </thead>
-      <tbody>{listItem}</tbody>
-    </table>
+    <div style={{ maxHeight: "20em" }}>
+      <table className="w-full text-xl text-left border-collapse ">
+        <thead
+          className="bg-white text-zinc-600"
+          style={{ position: "sticky", top: 0, zIndex: 1 }}
+        >
+          <tr>
+            <th className="px-2" width="5%"></th>
+            <th width="55%">Task Name</th>
+            <th width="20%">Deadline</th>
+            <th width="20%">Status</th>
+          </tr>
+        </thead>
+        <tbody>{listItem}</tbody>
+      </table>
+    </div>
   );
 }
