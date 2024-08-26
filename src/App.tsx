34b28bd6eb8947a0fd8fe238e346492720ca230e
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-26 21:09:42 +0700

    refactor : removing cluttered state and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,21 @@
 import { Header } from "./component/Header";
-import { useState, useEffect } from "react";
 import TasksList from "./component/Tasks";
 import TaskForm from "./component/Tasks/ModalTaskForm";
+import { useTaskForm } from "./stores/taskStore";
 
 function App() {
-  const [addTaskForm, setAddTaskForm] = useState(false);
-  const [tasks, setTasks] = useState(() => {
-    const tasksFromLocalStorage = localStorage.getItem("tasks");
-    return tasksFromLocalStorage ? JSON.parse(tasksFromLocalStorage) : [];
-  });
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
-
-
+  const [form] = useTaskForm((state) => [state.form]);
 
   return (
     <>
       <div className="flex justify-center w-full bg-gray-300  h-[100vh] font-kreon text-primary">
         <div className="px-3 overflow-hidden laptop:w-1/2">
           <Header />
-          <TasksList
-            addTaskForm={addTaskForm}
-            setAddTaskForm={setAddTaskForm}
-          />
+          <TasksList />
         </div>
 
-        {addTaskForm && (
-          <TaskForm
-            setAddTaskForm={setAddTaskForm}
-          />
+        {form && (
+          <TaskForm />
         )}
       </div>
     </>
