1dba52fd829654962aa6e87f4f8a4618b8149647
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-26 22:01:15 +0700

    fix : delete props

--- src/component/Tasks/ModalTaskForm.tsx
+++ src/component/Tasks/ModalTaskForm.tsx
@@ -1,13 +1,9 @@
 import FormInput from "./taskFormInput";
-export default function TaskForm({
-  setAddTaskForm,
-}) {
+export default function TaskForm() {
   return (
     <div className="absolute flex flex-col items-center justify-center w-screen h-screen">
       <div className="py-6 px-7 laptop:w-[36em] w-full z-20	">
-        <FormInput
-          setAddTaskForm={setAddTaskForm}
-        />
+        <FormInput />
       </div>
     </div>
   );
