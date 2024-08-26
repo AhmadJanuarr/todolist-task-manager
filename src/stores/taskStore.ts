eb3cca5ebf2924deadb1a13d23156edf3d12fe93
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-26 22:03:36 +0700

    fix : fixed bug on action in store

--- src/stores/taskStore.ts
+++ src/stores/taskStore.ts
@@ -1,19 +1,37 @@
 import { StateCreator, create } from "zustand";
-import { Task, TasksTypes } from "../types/task";
+import { TasksTypes } from "../types/task";
 import { persist } from "zustand/middleware";
 
 const createTaskStore: StateCreator<TasksTypes> = (set) => ({
     tasks: [],
     upTasks: () => set((state) => ({ tasks: [...state.tasks] })),
-    action:
-    {
-        addTask: (newTask) => set((state) => ({ tasks: [...state.tasks, newTask] })),
-        deleteTask: (id) => set((state) => ({ ...state, tasks: state.tasks.filter((task) => task.id !== id) })),
-        updateTask: (id, updateTask) => set((state) => ({ tasks: state.tasks.map((task) => task.id === id ? { ...task, ...updateTask } : task) }))
-    }
-})
+    addTask: (newTask) => set((state) => ({ tasks: [...state.tasks, newTask] })),
+    deleteTask: (id) =>
+        set((state) => ({
+            ...state,
+            tasks: state.tasks.filter((task) => task.id !== id),
+        })),
+    updateTask: (id, updateTask) =>
+        set((state) => ({
+            tasks: state.tasks.map((task) =>
+                task.id === id ? { ...task, ...updateTask } : task
+            ),
+        })),
+});
 
 export const useTasksStore = create<TasksTypes>()(
     persist(createTaskStore, {
-        name: "tasks"
-    }))
+        name: "tasks",
+    })
+);
+
+interface TaskFormTypes {
+    form: boolean;
+    upForm: () => void;
+}
+const createTaskForm: StateCreator<TaskFormTypes> = (set) => ({
+    form: false,
+    upForm: () => set((state) => ({ form: !state.form })),
+});
+
+export const useTaskForm = create<TaskFormTypes>(createTaskForm);
