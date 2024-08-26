782273e7b9884eec467ff9d477076aa221526997
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-26 22:01:49 +0700

    fix : delete props

--- src/component/Tasks/task.tsx
+++ src/component/Tasks/task.tsx
@@ -2,23 +2,13 @@ import DesktopComponent from "./ResponsiveComponent/DesktopComponent";
 import MobileComponent from "./ResponsiveComponent/MobileComponent";
 
 export default function Task({
-  tasks,
-  selectedTasksById,
-  setSelectedTasksById,
+
 }) {
   return (
     <div className="overflow-y-scroll ">
       <div className="w-full h-[26em] laptop:h-[20em]">
-        <DesktopComponent
-          tasks={tasks}
-          setSelectedTasksById={setSelectedTasksById}
-          selectedTasksById={selectedTasksById}
-        />
-        <MobileComponent
-          tasks={tasks}
-          setSelectedTasksById={setSelectedTasksById}
-          selectedTasksById={selectedTasksById}
-        />
+        <DesktopComponent />
+        <MobileComponent />
       </div>
     </div>
   );
