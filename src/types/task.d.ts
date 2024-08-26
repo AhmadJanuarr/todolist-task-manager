a122d1fa10696cf795a35175003ca6f855bb0c3b
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-26 22:13:02 +0700

    fix : change data type

--- src/types/task.d.ts
+++ src/types/task.d.ts
@@ -1,19 +1,18 @@
 export interface TasksTypes {
     tasks: Task[];
-    action: Action;
     upTasks: () => void;
-}
-
-export type Task = {
-    id: number;
-    task: string;
-    deadline: Date;
-    selected: boolean;
-    status: "Not Started" | "In Progress" | "Done";
-};
-
-export type Action = {
     addTask: (newTask: Task) => void;
     deleteTask: (id: number) => void;
     updateTask: (id: number, updateTask: Partial<Task>) => void;
 }
+
+export type Task = {
+    id: number;
+    title: string;
+    deadline: HTMLInputTypeAttribute;
+    selected?: boolean;
+    status: string
+};
+
+
+type HTMLInputTypeAttribute = "number" | "search" | "button" | "time" | "image" | "text" | "checkbox" | "color" | "date" | "datetime-local" | "email" | "file" | "hidden" | "month" | "password" | "radio" | "range" | (string & {})
