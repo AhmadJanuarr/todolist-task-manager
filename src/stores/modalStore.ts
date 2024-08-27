474b54b9f5a78f380c21c620a97ea49db57c3900
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-28 01:08:29 +0700

    feat : add modal store

---
+++ src/stores/modalStore.ts
@@ -0,0 +1,10 @@
+import { create, StateCreator } from "zustand";
+import { TaskFormProps } from "../types/task";
+
+const createModalStore: StateCreator<TaskFormProps> = (set) => ({
+    modalAddTask: false,
+    openModalAddTask: () => set({ modalAddTask: true, }),
+    closeModalAddTask: () => set({ modalAddTask: false }),
+});
+
+export const useModalStore = create<TaskFormProps>(createModalStore);
