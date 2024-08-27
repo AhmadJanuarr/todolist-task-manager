ebb2947dc8b43d81bd25557d4ae477fc6d0d39c3
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-28 01:12:09 +0700

    feat : update UI

--- src/component/Tasks/ResponsiveComponent/DesktopComponent.tsx
+++ src/component/Tasks/ResponsiveComponent/DesktopComponent.tsx
@@ -1,9 +1,30 @@
-import { useTasksStore } from "../../../stores/taskStore";
+import {
+  useSelectedStore,
+  useTasksStore,
+  useFilteredStore,
+} from "../../../stores/taskStore";
 
 export default function DesktopComponent() {
-
   const [tasks] = useTasksStore((state) => [state.tasks]);
-  const listItem = tasks.map((task) => {
+  const [selectedId, toggleSelected] = useSelectedStore((state) => [
+    state.selectedId,
+    state.toggleSelected,
+  ]);
+  const [filtered] = useFilteredStore((state) => [
+    state.filtered,
+    state.filteredName,
+  ]);
+
+  let filteredTasks;
+  if (filtered === "All Tasks") {
+    filteredTasks = tasks;
+  } else {
+    filteredTasks = tasks.filter((task) =>
+      task.status.toLowerCase().includes(filtered.toLowerCase())
+    );
+  }
+
+  const listItem = filteredTasks.map((task) => {
     const { id, title, deadline, status } = task;
 
     const truncatedTaskName =
@@ -22,35 +43,34 @@ export default function DesktopComponent() {
         <td className="px-2 py-2 text-gray" width="5%">
           <input
             type="checkbox"
-          // checked={ }
-          // onChange={ }
+            checked={selectedId.includes(id)}
+            onChange={() => toggleSelected(id)}
           />
         </td>
-        <td width="55%" className="py-2">
+        <td width="50%" className="py-2">
           {truncatedTaskName}
         </td>
         <td width="20%" className="py-2">
           {deadline}
         </td>
-        <td width="20%">
+        <td width="25%">
           <button className={statusClassName}>● {status}</button>
         </td>
       </tr>
     );
   });
-
   return (
-    <div style={{ maxHeight: "20em" }}>
+    <div>
       <table className="w-full text-xl text-left border-collapse ">
         <thead
           className="bg-white text-zinc-600"
           style={{ position: "sticky", top: 0, zIndex: 1 }}
         >
-          <tr>
-            <th style={{ width: '5%' }} className="px-2"></th>
-            <th style={{ width: '55%' }}>Task Name</th>
-            <th style={{ width: '20%' }}>Deadline</th>
-            <th style={{ width: '20%' }}>Status</th>
+          <tr className="text-[16px]">
+            <th style={{ width: "5%" }} className="px-2"></th>
+            <th style={{ width: "40%" }}>Task Name</th>
+            <th style={{ width: "20%" }}>Deadline</th>
+            <th style={{ width: "30%" }}>Status</th>
           </tr>
         </thead>
         <tbody>{listItem}</tbody>
