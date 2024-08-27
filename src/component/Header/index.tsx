8883f032088875295a12077da12de8eea3022959
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-28 01:11:10 +0700

    feat : change font

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -1,12 +1,12 @@
 import img from "../../../public/image/img1.png";
-export function Header() {
+export default function Header() {
   return (
     <>
       <div className="container w-full h-30 flex py-3">
         <img src={img} alt="svg" className="w-[100px] h-[100px] self-center" />
         <div className="flex flex-col justify-center pl-5">
-          <h1 className="text-2xl">My Tasks</h1>
-          <p className="text-md text-green ">
+          <h1 className="text-2xl font-bold">My Tasks</h1>
+          <p className="text-md text-green font-reguler">
             Manage and Prioritize Your Todolist Tasks Effectively
           </p>
         </div>
