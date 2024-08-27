437aaaaa58a9fb2d6756297ac964f46e9109b426
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-28 01:06:29 +0700

    feat : add color in theme pallete

---
+++ src/theme.palette.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+import { lime, purple } from "@mui/material/colors";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#80cbc4",
+    },
+    secondary: {
+      main: "#ef9a9a",
+    },
+  },
+});
