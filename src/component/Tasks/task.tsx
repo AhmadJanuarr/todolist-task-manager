51c5a2146b3e9e75bf1ed1a07f8f73851397add4
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-28 23:23:29 +0700

    refactor : change size style height

--- src/component/Tasks/task.tsx
+++ src/component/Tasks/task.tsx
@@ -4,7 +4,7 @@ import MobileComponent from "./ResponsiveComponent/MobileComponent";
 export default function Task() {
   return (
     <div className="scrollbar-hide overflow-y-auto">
-      <div className={`w-full h-[26em] desktop:h-[30em] laptop:h-[19em]`}>
+      <div className={`w-full h-[27em] desktop:h-[30em] laptop:h-[22em]`}>
         <DesktopComponent />
         <MobileComponent />
       </div>
