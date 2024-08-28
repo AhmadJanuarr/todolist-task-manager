227439dea6143301906fe4498aabf66127e85de4
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-28 20:31:05 +0700

    fix : fix the code that has a bug

--- src/component/Tasks/index.tsx
+++ src/component/Tasks/index.tsx
@@ -4,8 +4,8 @@ import {
   useFilteredStore,
 } from "../../stores/taskStore";
 import { useState } from "react";
-import Alert from '@mui/material/Alert';
-import Stack from '@mui/material/Stack';
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
@@ -14,18 +14,25 @@ import DialogTitle from "@mui/material/DialogTitle";
 import Button from "@mui/material/Button";
 import Task from "./task";
 import MenuItem from "@mui/material/MenuItem";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
 import { useModalStore } from "../../stores/modalStore";
 import { useShallow } from "zustand/react/shallow";
+import { useDarkModeStore } from "../../stores/darkModeStore";
 
 function TaskHeader() {
   const [open, setOpen] = useState(false);
-  const [alert, setAlert] = useState(false)
+  const [alert, setAlert] = useState(false);
   const [selectedId] = useSelectedStore((state) => [state.selectedId]);
-  const [openModalAddTask] = useModalStore(useShallow((state) => [state.openModalAddTask]));
-  const [filtered, filteredName] = useFilteredStore((state) => [state.filtered, state.filteredName]);
+  const [isDarkMode] = useDarkModeStore((state) => [state.isDarkMode]);
+  const [openModalAddTask] = useModalStore(
+    useShallow((state) => [state.openModalAddTask])
+  );
+  const [filtered, filteredName] = useFilteredStore((state) => [
+    state.filtered,
+    state.filteredName,
+  ]);
 
   const handleDeleteOpen = () => {
     setOpen(true);
@@ -33,33 +40,27 @@ function TaskHeader() {
 
   const handleClose = () => {
     setOpen(false);
-    setAlert(false)
+    setAlert(false);
   };
 
   const deleteTask = useTasksStore((state) => state.deleteTask);
 
   const handleDelete = () => {
     if (selectedId.length === 0) {
-      setAlert(true)
+      setAlert(true);
     } else {
       deleteTask(selectedId);
       setOpen(false);
     }
   };
 
-
   return (
     <>
       <div className="flex justify-between w-full py-5">
-        <FormControl size="small" sx={{ minWidth: 120 }}>
-          <InputLabel id="demo-simple-select-label">Filter</InputLabel>
+        <FormControl size="small" sx={{ minWidth: 120, bgcolor: `${isDarkMode && "primary.main"}`, fontWeight: 'light' }}>
           <Select
-            labelId="demo-select-small-label"
-            id="demo-select-small"
-            label="Filter"
             value={filtered}
             onChange={(e) => filteredName(e.target.value)}
-            autoWidth
           >
             <MenuItem value="All Tasks">Show All Tasks</MenuItem>
             <MenuItem value="Not Progress">Show Not Progress</MenuItem>
@@ -81,18 +82,18 @@ function TaskHeader() {
         </div>
       </div>
 
-
       <Dialog
         open={open}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description" classes={{ paper: "w-full" }}
+        aria-describedby="alert-dialog-description"
+        classes={{ paper: "w-full" }}
       >
         <DialogTitle id="alert-dialog-title">
           {"Confirmation of Deletion"}
         </DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description" >
+          <DialogContentText id="alert-dialog-description">
             Are you sure you want to delete this task?
           </DialogContentText>
         </DialogContent>
@@ -103,11 +104,10 @@ function TaskHeader() {
           </Button>
         </DialogActions>
         {alert && (
-          <Stack sx={{ width: '100%' }} spacing={2}>
+          <Stack sx={{ width: "100%" }} spacing={2}>
             <Alert severity="error">Please select at least one task.</Alert>
           </Stack>
         )}
-
       </Dialog>
     </>
   );
@@ -116,7 +116,7 @@ function TaskHeader() {
 export default function TasksList() {
   return (
     <>
-      <div className="w-full desktop:h-[42em] laptop:h-[20em] rounded laptop:p-7 px-4 ">
+      <div className="w-full desktop:h-[38em] rounded ">
         <hr />
         <TaskHeader />
         <Task />
