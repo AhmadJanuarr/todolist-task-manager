2321b2cee7badefda95d21b7d4e507787641d06b
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-28 20:26:05 +0700

    fix : fix bug

--- src/stores/modalStore.ts
+++ src/stores/modalStore.ts
@@ -3,7 +3,7 @@ import { TaskFormProps } from "../types/task";
 
 const createModalStore: StateCreator<TaskFormProps> = (set) => ({
     modalAddTask: false,
-    openModalAddTask: () => set({ modalAddTask: true, }),
+    openModalAddTask: () => set({ modalAddTask: true }),
     closeModalAddTask: () => set({ modalAddTask: false }),
 });
 
