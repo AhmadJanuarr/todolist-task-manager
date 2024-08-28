781c86953536713ffc8089d4851875ecf1ea097d
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-28 20:25:16 +0700

    feat : add file component footer

---
+++ src/component/Footer/index.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer className="w-full bg-gray-400 flex justify-center items-center py-1">
+            <p className="text-center text-sm">
+                © 2024 madds. All rights reserved.
+            </p>
+        </footer>
+    )
+
+}
