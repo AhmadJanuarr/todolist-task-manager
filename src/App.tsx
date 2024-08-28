5d8faaa8fe0cb717a2d79240d7819e68ad7409d6
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-28 20:24:22 +0700

    feat : add components footer and m fix code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import Footer from "./component/Footer";
 import Header from "./component/Header";
 import TasksList from "./component/Tasks";
 import TaskForm from "./component/Tasks/ModalTaskForm";
+import { useDarkModeStore } from "./stores/darkModeStore";
 import { useModalStore } from "./stores/modalStore";
 
 function App() {
   const [modalAddTask] = useModalStore((state) => [state.modalAddTask]);
+  const [isDarkMode] = useDarkModeStore((state) => [state.isDarkMode]);
+  console.log(isDarkMode);
   return (
     <>
-      <div className="flex justify-center w-full bg-gray-300  h-[100vh] font-kreon text-primary">
-        <div className="px-3 overflow-hidden laptop:w-1/2">
+      <div
+        className={`flex justify-center w-full bg-gray-300 h-[100vh] font-kreon ${isDarkMode ? "bg-dark text-white" : ""
+          }`}
+      >
+        <div className="px-3 overflow-hidden laptop:w-[60%]">
           <Header />
           <TasksList />
+          <Footer />
         </div>
         {modalAddTask && <TaskForm />}
       </div>
