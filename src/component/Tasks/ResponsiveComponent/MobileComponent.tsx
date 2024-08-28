c241b553c755d5249b8869b5a02fe4c34e0c6984
MAD <ahmadjanuaramri2015@gmail.com>
2024-08-28 20:34:10 +0700

    feat : added darkmode, added filter feature on tasks and changed status to select

--- src/component/Tasks/ResponsiveComponent/MobileComponent.tsx
+++ src/component/Tasks/ResponsiveComponent/MobileComponent.tsx
@@ -1,9 +1,27 @@
-import { useTasksStore } from "../../../stores/taskStore";
+import { useDarkModeStore } from "../../../stores/darkModeStore";
+import { useFilteredStore, useTasksStore } from "../../../stores/taskStore";
 
 export default function MobileComponent() {
-
   const [tasks] = useTasksStore((state) => [state.tasks]);
-  const listItem = tasks.map((task) => {
+  const [filtered] = useFilteredStore((state) => [
+    state.filtered,
+    state.filteredName,
+  ]);
+
+  let filteredTasks;
+
+  if (filtered === "All Tasks") {
+    filteredTasks = tasks;
+  } else {
+    filteredTasks = tasks.filter((task) =>
+      task.status.toLowerCase().includes(filtered.toLowerCase())
+    );
+  }
+  const [isDarkMode] = useDarkModeStore((state) => [state.isDarkMode]);
+
+
+
+  const listItem = filteredTasks.map((task) => {
     const { id, title, deadline, status } = task;
 
     const className = "px-2 text-center border rounded-xl";
@@ -16,27 +34,23 @@ export default function MobileComponent() {
 
     return (
       <table
-        className="w-full mb-5 text-left border-separate border-gray-300 rounded laptop:hidden text-md bg-grayDark border-spacing-2"
+        className={`w-full mb-5 text-left border-separate border-gray-300 rounded text-md ${isDarkMode ? "bg-grayDark text-dark" : "bg-grayDark"} border-spacing-2 `}
         key={id}
       >
         <tbody>
           <tr>
             <td className="px-2 text-zinc-400" width={"5%"} rowSpan={4}>
-              <input
-                type="checkbox"
-              />
+              <input type="checkbox" />
             </td>
           </tr>
           <tr className="text-[12px]">
             <th className="text-zinc-400">Task Name</th>
             <td width={"70%"}>{title}</td>
           </tr>
-
           <tr className=" text-[12px] ">
             <th className="text-zinc-400">Deadline</th>
             <td>{deadline}</td>
           </tr>
-
           <tr className="text-[12px]">
             <th className="text-zinc-400">Status</th>
             <td>
@@ -48,5 +62,5 @@ export default function MobileComponent() {
     );
   });
 
-  return <div> {listItem}</div>;
+  return <div className="laptop:hidden"> {listItem}</div>;
 }
